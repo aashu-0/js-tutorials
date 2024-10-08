796c82f837279b049fd8818fbc2b49537cade1f3
aashu <157953935+aashu-0@users.noreply.github.com>
2024-10-08 19:38:08 +0000

    comparion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+// try to avoid having these type of conversion in your code
+
+// === (strict check)
+
+console.log("2"===2);
