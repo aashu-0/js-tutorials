c9bc61deb6a894cb126078d033152810edd83b46
aashu <157953935+aashu-0@users.noreply.github.com>
2024-10-08 20:22:39 +0000

    stack/heap added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,25 +6,25 @@
 // Javascript is a dynamically typed language 
 // because data type will automatically assigned at the time of compilation or code execution.
 
-const score = 100
-const scoreValue = 100.3
-console.log(typeof scoreValue)
+// const score = 100
+// const scoreValue = 100.3
+// console.log(typeof scoreValue)
 
-const isLoggedIn = false
-const outsideTemp = null
-console.log(typeof outsideTemp);
+// const isLoggedIn = false
+// const outsideTemp = null
+// console.log(typeof outsideTemp);
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(anotherId)
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(anotherId)
 
-// id and anotherId are different 
-console.log(id == anotherId)
+// // id and anotherId are different 
+// console.log(id == anotherId)
 
-const bigNumber = 12345678998456678n
-console.log(typeof bigNumber);
+// const bigNumber = 12345678998456678n
+// console.log(typeof bigNumber);
 
 
 // non-primitive or (reference) datatypes
@@ -32,14 +32,41 @@ console.log(typeof bigNumber);
 // Array, Objects, Functions
 
 
-const heros = ["spider-man", "captain-america", "thor","bat-man"]
-let myObj= {
-    name: "aashutosh",
-    age: "18"
+// const heros = ["spider-man", "captain-america", "thor","bat-man"]
+// let myObj= {
+//     name: "aashutosh",
+//     age: "18"
 
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world!")
+// }
+// console.log(typeof myFunction)  // datatype of function is called as function object.
+
+
+// **************************** Memory in js ************************
+
+// Stack (used in primitive ), Heap(used in Non-Primitive)
+// whenever stack memory is used, we get a copy of variable declared
+//  whenever heap, we get reference of original values, means any changes will be reflected in original values
+
+let myOriginalName = "aashutoshmishra"
+
+let anothername = myOriginalName
+anothername = "aashu"
+
+console.log(myOriginalName);
+console.log(anothername);
+
+let userOne = {
+    email: "user1@gmail.com",
+    upi: "user1@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello world!")
-}
-console.log(typeof myFunction)  // datatype of function is called as function object.
+let userTwo = userOne
+
+userTwo.email = "user2@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
