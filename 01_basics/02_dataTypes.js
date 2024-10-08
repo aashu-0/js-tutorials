0ca575f5998fc2024d018851f7457b4a0338bbfa
aashu <157953935+aashu-0@users.noreply.github.com>
2024-10-08 18:16:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version 
+// as there diiferences in new js and old js things like classes, modules, arroe functions etc.
+
+// alert(3 +3) give error!! as we're using nodejs not browser
+
+// code readability should be high
+
+
+let name = "Aashutosh" //string
+let age = 18 //integers
+let isLoggedIn = false //bool
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, sepecially defined
+// undefined => value not assigned yet
+// symbol =>  used for uniqueness
+
+//objects
+
+console.log(typeof undefined);
+console.log(typeof null);
+
