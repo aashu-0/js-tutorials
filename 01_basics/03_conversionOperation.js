1075c4f6deec459e829f1c078d99dd22263d0eb8
aashu <157953935+aashu-0@users.noreply.github.com>
2024-10-08 18:36:55 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// conversion
+// "33" => 33
+// "33abc"=> NaN
+// true =>1; false => 0
+
+// let isLoggedIn = "aashutosh"
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsloggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// ""(empty string) => false
+// "aashutosh"(any string) => true
+
+let someNumber = 33
+
+let someNumberInString = String(someNumber)
+console.log(someNumberInString);
+console.log(typeof someNumberInString)
